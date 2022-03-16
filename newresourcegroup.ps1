f0072511643a9c55fca0665a9b224752ec4e9804
kpolisetty-i3 <67918648+kpolisetty-i3@users.noreply.github.com>
2022-03-16 13:34:34 -0400

    Update newresourcegroup.ps1

--- newresourcegroup.ps1
+++ newresourcegroup.ps1
@@ -1,3 +1,3 @@
 Connect-AzAccount -UseDeviceAuthentication | Out-null
 Select-AzSubscription -Subscription 'Pay-As-You-Go Dev/Test'
-New-AzStorageAccount -Location 'westus' -Name 'apiteststorage' -ResourceGroupName 'rg-iDAPv2' -SkuName Standard_LRS -EnableHttpsTrafficOnly $true -EnableHierarchicalNamespace $true -AllowBlobPublicAccess $true -Kind StorageV2 -Tag @{'Created By'='Krishna Polisetty'} | Out-Null
+New-AzStorageAccount -Location 'westus' -Name 'i3idapv2apiteststorage' -ResourceGroupName 'rg-iDAPv2' -SkuName Standard_LRS -EnableHttpsTrafficOnly $true -EnableHierarchicalNamespace $true -AllowBlobPublicAccess $true -Kind StorageV2 -Tag @{'Created By'='Krishna Polisetty'} | Out-Null
