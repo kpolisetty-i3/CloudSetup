0057f908323121d94b10f4a5c51b1d2d749a3af1
kpolisetty-i3 <67918648+kpolisetty-i3@users.noreply.github.com>
2022-03-21 18:27:33 -0400

    Update newresourcegroup.ps1

--- newresourcegroup.ps1
+++ newresourcegroup.ps1
@@ -1,3 +1,5 @@
-Connect-AzAccount -UseDeviceAuthentication | Out-null
-Select-AzSubscription -Subscription 'Pay-As-You-Go Dev/Test'
+
+$AzTent = (Get-AzContext).Tenant
+$subID = (Get-AzSubscription).SubscriptionId
+Write-Host "the new value is $(AzTent)" -ForegroundColor Yellow
 New-AzStorageAccount -Location 'westus' -Name 'i3idapv2apiteststorage' -ResourceGroupName 'rg-iDAPv2' -SkuName Standard_LRS -EnableHttpsTrafficOnly $true -EnableHierarchicalNamespace $true -AllowBlobPublicAccess $true -Kind StorageV2 -Tag @{'Created By'='Krishna Polisetty'} | Out-Null
