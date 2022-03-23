5c42db46cb91665c7471e0754b148606934529e2
kpolisetty-i3 <67918648+kpolisetty-i3@users.noreply.github.com>
2022-03-23 10:34:03 -0400

    Update newresourcegroup.ps1

--- newresourcegroup.ps1
+++ newresourcegroup.ps1
@@ -1,6 +1,6 @@
 
 $User = "kpolisetty@i3its.com"
-$PWord = ConvertTo-SecureString -String "$Vinayaka6" -AsPlainText -Force
+$PWord = ConvertTo-SecureString -String '$Vinayaka6' -AsPlainText -Force
 $subscription = "f3323769-d6c9-476b-9255-911aeb84b3ed"
 $Credential = New-Object -TypeName "System.Management.Automation.PSCredential" -ArgumentList $User,$PWord
 Connect-AzAccount -Credential $Credential -Subscription $subscription
