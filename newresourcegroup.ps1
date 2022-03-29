77d90ffb7cde2776ee4f5319fcb482928b4cb640
kpolisetty-i3 <67918648+kpolisetty-i3@users.noreply.github.com>
2022-03-29 08:28:34 -0400

    Update newresourcegroup.ps1

--- newresourcegroup.ps1
+++ newresourcegroup.ps1
@@ -1,7 +1,2 @@
 
-$User = "kpolisetty@i3its.com"
-$PWord = ConvertTo-SecureString -String '$Vinayaka6' -AsPlainText -Force
-$subscription = "f3323769-d6c9-476b-9255-911aeb84b3ed"
-$Credential = New-Object -TypeName "System.Management.Automation.PSCredential" -ArgumentList $User,$PWord
-Connect-AzAccount -Credential $Credential -Subscription $subscription
 New-AzStorageAccount -Location 'westus' -Name 'i3idapv2apiteststorage' -ResourceGroupName 'rg-iDAPv2' -SkuName Standard_LRS -EnableHttpsTrafficOnly $true -EnableHierarchicalNamespace $true -AllowBlobPublicAccess $true -Kind StorageV2 -Tag @{'Created By'='Krishna Polisetty'} | Out-Null
